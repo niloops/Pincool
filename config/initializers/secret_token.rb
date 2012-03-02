18e565cbac479dfea4bbfe3c8358ad6fb6fdb36a
lilu <lilu.life@gmail.com>
2012-03-02 13:55:42 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pincool::Application.config.secret_token = 'c05552ead145abe3ced82c3278f6b460309a904cc74a5122db180b5e4e1fcdad5b65ab943dd77f8655fd7086986653a36bf7c65c62a303efe3447dd725cfbcb6'
