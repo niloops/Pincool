d86dc9367fa1f7806ff5e37ad2da7ae35519db50
lilu <lilu.life@gmail.com>
2012-03-02 22:05:17 +0800

    capistrano to deploy

---
+++ config/deploy.rb
@@ -0,0 +1,44 @@
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require "rvm/capistrano"
+set :rvm_ruby_string, 'default'
+require 'bundler/capistrano'
+
+set :application, "Pincool"
+set :repository,  "git@github.com:Lilu/Pincool.git"
+set :user, "dev"
+set :ssh_options, {:forward_agent => true}
+set :deploy_via, :remote_cache
+set :deploy_to, "/var/pincool"
+set :use_sudo, false
+
+role :web, "pincool.me"                          
+role :app, "pincool.me"                          
+role :db,  "pincool.me", :primary => true 
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{File.join(current_path, 'tmp', 'restart.txt')}"
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
