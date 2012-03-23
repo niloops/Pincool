26ccee32cbd882c8b20ff7f64435520bececd73b
lilu <lilu.life@gmail.com>
2012-03-23 14:36:38 +0800

    add target  _blank for external link

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   def provider_tag(user)
     link_to image_tag("provider/#{user.current_auth.provider}.png"),
-    user.current_auth.link_url
+    user.current_auth.link_url, target: '_blank'
   end
 end
